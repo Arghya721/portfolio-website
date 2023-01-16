f67c9808be531a0dd0ef83b76ef313118156efa1
Arghya721 <arghyadeep.ghosh@bobble.ai>
2023-01-16 23:16:22 +0530

    Added Skills Section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,236 +1,114 @@
 import { Progress, Grid, Text, Spacer } from "@nextui-org/react";
 import { Box } from "./Box.js";
 
-export const Skills = () => (
-  <Box css={{ px: "$50", mt: "$8", "@xsMax": { px: "$10" } }}>
-    <Grid.Container justify="center">
-      <Text
-        css={{
-          textGradient: "45deg, $blue600 -20%, $pink600 50%",
-          fontSize: "70px",
-          "@xsMax": { fontSize: "50px" },
-        }}
-        weight="bold"
-      >
-        Skills
-      </Text>
-    </Grid.Container>
-    <Grid.Container justify="center" >
-      <Grid
-        xs={12}
-        xl={6}
-        css={{
-          padding: "0px 0px 0px 0px",
-        }}
-        justify="center"
-      >
-        <Text
-          css={{
-            textGradient: "45deg, $black -20%, $black 50%",
-            paddingLeft: "50px",
-            fontSize: "30px",
-            "@xsMax": { fontSize: "20px" },
-          }}
-          weight="bold"
-        >
-          C/C++
-          <Spacer y={1} />
-          <Progress
-            value={90}
-            color="success"
-            css={{ width: "500px", "@xsMax": { width: "300px" } }}
-          />
-        </Text>
-      </Grid>
+export const Skills = () => {
+  const skills = [
+    {
+      name: "C++",
+      value: 70,
+      color: "warning",
+    },
+    {
+      name: "Golang",
+      value: 90,
+      color: "primary",
+    },
+    {
+      name: "Python",
+      value: 60,
+      color: "warning",
+    },
+    {
+      name: "JavaScript",
+      value: 80,
+      color: "secondary",
+    },
+    {
+      name: "NodeJS",
+      value: 80,
+      color: "primary",
+    },
+    {
+      name: "Flask",
+      value: 50,
+      color: "gradient",
+    },
+    {
+      name: "ReactJS",
+      value: 60,
+      color: "primary",
+    },
+    {
+      name: "MySQL",
+      value: 70,
+      color: "gradient",
+    },
+    {
+      name: "MongoDB",
+      value: 60,
+      color: "warning",
+    },
+    {
+      name: "Docker",
+      value: 60,
+      color: "secondary",
+    },
+    {
+      name: "GIT",
+      value: 80,
+      color: "gradient",
+    },
+    {
+      name: "AWS",
+      value: 60,
+      color: "warning",
+    },
+  ];
 
-      <Grid
-        xs={12}
-        xl={6}
-        css={{
-          padding: "0px 0px 0px 0px",
-        }}
-        justify="center"
-      >
+  return (
+    <Box css={{ px: "$50", mt: "$8", "@xsMax": { px: "$10" } }}>
+      <Grid.Container justify="center">
         <Text
           css={{
-            textGradient: "45deg, $black -20%, $black 50%",
-            paddingLeft: "50px",
-            fontSize: "30px",
-            "@xsMax": { fontSize: "20px" },
+            textGradient: "45deg, $blue600 -20%, $pink600 50%",
+            fontSize: "70px",
+            "@xsMax": { fontSize: "50px" },
           }}
           weight="bold"
         >
-          Golang
-          <Spacer y={1} />
-          <Progress
-            value={80}
-            color="success"
-            css={{ width: "500px", "@xsMax": { width: "300px" } }}
-          />
+          Skills
         </Text>
-      </Grid>
-
-      <Grid
-        xs={12}
-        xl={6}
-        css={{
-          padding: "0px 0px 0px 0px",
-        }}
-        justify="center"
-      >
-        <Text
-          css={{
-            textGradient: "45deg, $black -20%, $black 50%",
-            paddingLeft: "50px",
-            fontSize: "30px",
-            "@xsMax": { fontSize: "20px" },
-          }}
-          weight="bold"
-        >
-          Python
-          <Spacer y={1} />
-          <Progress
-            value={70}
-            color="success"
-            css={{ width: "500px", "@xsMax": { width: "300px" } }}
-          />
-        </Text>
-      </Grid>
-
-      <Grid
-        xs={12}
-        xl={6}
-        css={{
-          padding: "0px 0px 0px 0px",
-        }}
-        justify="center"
-      >
-        <Text
-          css={{
-            textGradient: "45deg, $black -20%, $black 50%",
-            paddingLeft: "50px",
-            fontSize: "30px",
-            "@xsMax": { fontSize: "20px" },
-          }}
-          weight="bold"
-        >
-          React
-          <Spacer y={1} />
-          <Progress
-            value={60}
-            color="success"
-            css={{ width: "500px", "@xsMax": { width: "300px" } }}
-          />
-        </Text>
-      </Grid>
-
-      <Grid
-        xs={12}
-        xl={6}
-        css={{
-          padding: "0px 0px 0px 0px",
-        }}
-        justify="center"
-      >
-        <Text
-          css={{
-            textGradient: "45deg, $black -20%, $black 50%",
-            paddingLeft: "50px",
-            fontSize: "30px",
-            "@xsMax": { fontSize: "20px" },
-          }}
-          weight="bold"
-        >
-          Node.js
-          <Spacer y={1} />
-          <Progress
-            value={50}
-            color="success"
-            css={{ width: "500px", "@xsMax": { width: "300px" } }}
-          />
-        </Text>
-      </Grid>
-
-      <Grid
-        xs={12}
-        xl={6}
-        css={{
-          padding: "0px 0px 0px 0px",
-        }}
-        justify="center"
-      >
-        <Text
-          css={{
-            textGradient: "45deg, $black -20%, $black 50%",
-            paddingLeft: "50px",
-            fontSize: "30px",
-            "@xsMax": { fontSize: "20px" },
-          }}
-          weight="bold"
-        >
-          Express
-          <Spacer y={1} />
-          <Progress
-            value={40}
-            color="success"
-            css={{ width: "500px", "@xsMax": { width: "300px" } }}
-          />
-        </Text>
-      </Grid>
-
-      <Grid
-        xs={12}
-        xl={6}
-        css={{
-          padding: "0px 0px 0px 0px",
-        }}
-        justify="center"
-      >
-        <Text
-          css={{
-            textGradient: "45deg, $black -20%, $black 50%",
-            paddingLeft: "50px",
-            fontSize: "30px",
-            "@xsMax": { fontSize: "20px" },
-          }}
-          weight="bold"
-        >
-          MongoDB
-          <Spacer y={1} />
-          <Progress
-            value={30}
-            color="success"
-            css={{ width: "500px", "@xsMax": { width: "300px" } }}
-          />
-        </Text>
-      </Grid>
-
-      <Grid
-        xs={12}
-        xl={6}
-        css={{
-          padding: "0px 0px 0px 0px",
-        }}
-        justify="center"
-      >
-        <Text
-          css={{
-            textGradient: "45deg, $black -20%, $black 50%",
-            paddingLeft: "50px",
-            fontSize: "30px",
-            "@xsMax": { fontSize: "20px" },
-          }}
-          weight="bold"
-        >
-          Mongoose
-          <Spacer y={1} />
-          <Progress
-            value={20}
-            color="success"
-            css={{ width: "500px", "@xsMax": { width: "300px" } }}
-          />
-        </Text>
-      </Grid>
-    </Grid.Container>
-  </Box>
-);
+      </Grid.Container>
+      <Grid.Container justify="center">
+        {skills.map((skill) => (
+          <Grid
+            xs={12}
+            xl={6}
+            css={{
+              padding: "0px 0px 0px 0px",
+            }}
+            justify="center"
+          >
+            <Text
+              css={{
+                textGradient: "45deg, $black -20%, $black 50%",
+                paddingLeft: "50px",
+                fontSize: "30px",
+                "@xsMax": { fontSize: "20px" },
+              }}
+              weight="bold"
+            >
+              {skill.name}
+              <Spacer y={1} />
+              <Progress
+                value={skill.value}
+                color={skill.color}
+                css={{ width: "500px", "@xsMax": { width: "300px" } }}
+              />
+            </Text>
+          </Grid>
+        ))}
+      </Grid.Container>
+    </Box>
+  );
+};
