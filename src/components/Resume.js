1cd02266420bfa3da065a56caa410fab415904c2
Arghya721 <imarghyadeep@gmail.com>
2023-01-30 02:31:32 +0530

    Maded Changes on Resume Section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,4 @@
-import { Progress, Grid, Text, Spacer } from "@nextui-org/react";
+import { Progress, Grid, Text, Spacer, css } from "@nextui-org/react";
 import { Box } from "./Box.js";
 import "../styles/Resume.css";
 
@@ -17,110 +17,163 @@ export const Resume = () => {
           Resume
         </Text>
       </Grid.Container>
-      <Grid.Container justify="center" 
-        css = {{
-            paddingLeft: "2rem",
-            paddingRight: "2rem",
+      <Grid.Container
+        justify="center"
+        css={{
+          paddingLeft: "2rem",
+          paddingRight: "2rem",
         }}
-      
       >
         <Grid
-          xs={12} md={6}
+          xs={12}
+          md={6}
           css={{
             padding: "0px 0px 0px 0px",
           }}
           justify="center"
-          
         >
+          <div>
+            <Text h1>
+              Summary
+              <Spacer y={1} />
+              <Text className="resume-item" h5>
+                ARGHYADEEP GHOSH
+                <Text h5>
+                  I am a computer programmer and a web developer. I am seeking a
+                  role that will allow me to demonstrate my expertise and unlock
+                  my full potential in delivering value to the company.
+                </Text>
+                <Text
+                  h6
+                  css={{
+                    fontWeight: "bold",
+                    fontStyle: "italic",
+                  }}
+                >
+                  <li>Jhargram , West Bengal , India</li>
+                  <Spacer y={1} />
+                  <li>imarghyadeep@gmail.com</li>
+                </Text>
+              </Text>
+            </Text>
 
-            <div>
+            <Text h1>
+              Education
+              <Spacer y={1} />
+              <Text
+                className="resume-item"
+                h5
+                css={{
+                  marginBottom: "0px",
+                }}
+              >
+                COMPUTER SCIENCE AND ENGINEERING (B.TECH)
+                <Text h5>2019-2023</Text>
+                <h5
+                  style={{
+                    fontWeight: "bold",
+                    fontStyle: "italic",
+                  }}
+                >
+                  Kalinga Institute of Industrial Technology
+                </h5>
+                <h6
+                  style={{
+                    fontWeight: "bold",
+                    fontStyle: "italic",
+                  }}
+                >
+                  CGPA- 9.23/10
+                </h6>
+              </Text>
+              <h5
+                style={{
+                  marginTop: "0px",
+                  marginBottom: "0px",
+                }}
+                className="resume-item"
+              >
+                HIGHER SECONDARY
+
+                <Text h5>2019</Text>
+                <Text h5> Vidya Bharati Chinmaya Vidyalaya</Text>
+                <Text h5>Percentage-85%</Text>
+              </h5>
+
+              <h5
+                style={{
+                  marginTop: "0px",
+                }}
+                className="resume-item"
+              >
+                HIGH SCHOOL
+
+                <Text h5>2017</Text>
+                <Text h5> West End High School</Text>
+                <Text h5>Percentage-88%</Text>
+              </h5>
+            </Text>
+
             <Text h1 className="resume-item">
-            Summary
-            <h6 >
-              Hey there! I'm a Full Stack Developer from India. I'm passionate
-              about building web applications and learning new technologies.
-            </h6>
-          </Text>
+              Summary
+              <h6>
+                Hey there! I'm a Full Stack Developer from India. I'm passionate
+                about building web applications and learning new technologies.
+              </h6>
+            </Text>
 
-          <Text h1 className="resume-item">
-            Summary
-            <h6>
-              Hey there! I'm a Full Stack Developer from India. I'm passionate
-              about building web applications and learning new technologies.
-            </h6>
-          </Text>
-
-
-
-          <Text h1 className="resume-item">
-            Summary
-            <h6>
-              Hey there! I'm a Full Stack Developer from India. I'm passionate
-              about building web applications and learning new technologies.
-            </h6>
-          </Text>
-
-          <Text h1 className="resume-item">
-            Summary
-            <h6>
-              Hey there! I'm a Full Stack Developer from India. I'm passionate
-              about building web applications and learning new technologies.
-            </h6>
-          </Text>
-            </div>
-          
-
+            <Text h1 className="resume-item">
+              Summary
+              <h6>
+                Hey there! I'm a Full Stack Developer from India. I'm passionate
+                about building web applications and learning new technologies.
+              </h6>
+            </Text>
+          </div>
         </Grid>
 
         <Grid
-          xs={12} md={6}
+          xs={12}
+          md={6}
           css={{
             padding: "0px 0px 0px 0px",
           }}
           justify="center"
-          
         >
-
-            <div>
+          <div>
             <Text h1 className="resume-item">
-            Summary
-            <h6 >
-              Hey there! I'm a Full Stack Developer from India. I'm passionate
-              about building web applications and learning new technologies.
-            </h6>
-          </Text>
+              Summary
+              <h6>
+                Hey there! I'm a Full Stack Developer from India. I'm passionate
+                about building web applications and learning new technologies.
+              </h6>
+            </Text>
 
-          <Text h1 className="resume-item">
-            Summary
-            <h6>
-              Hey there! I'm a Full Stack Developer from India. I'm passionate
-              about building web applications and learning new technologies.
-            </h6>
-          </Text>
+            <Text h1 className="resume-item">
+              Summary
+              <h6>
+                Hey there! I'm a Full Stack Developer from India. I'm passionate
+                about building web applications and learning new technologies.
+              </h6>
+            </Text>
 
+            <Text h1 className="resume-item">
+              Summary
+              <h6>
+                Hey there! I'm a Full Stack Developer from India. I'm passionate
+                about building web applications and learning new technologies.
+              </h6>
+            </Text>
 
-
-          <Text h1 className="resume-item">
-            Summary
-            <h6>
-              Hey there! I'm a Full Stack Developer from India. I'm passionate
-              about building web applications and learning new technologies.
-            </h6>
-          </Text>
-
-          <Text h1 className="resume-item">
-            Summary
-            <h6>
-              Hey there! I'm a Full Stack Developer from India. I'm passionate
-              about building web applications and learning new technologies.
-            </h6>
-          </Text>
-
-            </div>
-         
-          </Grid>
-        
-
+            <Text h1 className="resume-item">
+              Summary
+              <h6>
+                Hey there! I'm a Full Stack Developer from India. I'm passionate
+                about building web applications and learning new technologies.
+              </h6>
+            </Text>
+          </div>
+        </Grid>
       </Grid.Container>
     </Box>
   );
