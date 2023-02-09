a969030797a303ca920aff535377708ec4d4bfe1
Arghya721 <imarghyadeep@gmail.com>
2023-02-10 00:19:59 +0530

    Added Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -17,41 +17,39 @@ import "react-vertical-timeline-component/style.min.css";
 import { MdOutlineCorporateFare } from "react-icons/md";
 import { FaSchool, FaUniversity } from "react-icons/fa";
 
-
-
 export const Experience = () => {
-
   const BobbleAI = [
     "Implemented Location-based targeting for Mint Keyboard Sticker Pack APIs using Golang.",
     "Implemented Redis to cache JSON data to reduce the latency of Mint Keyboard Sticker API which receives 10 million requests daily.",
-    "Created a Dashboard using React for Content Team to manage sticker packs of Mint Keyboard."
-  ]
+    "Created a Dashboard using React for Content Team to manage sticker packs of Mint Keyboard.",
+  ];
 
   const HighRadius = [
-    "Developed PDF and Excel parser to extract critical data from an invoice using Java and RegEx." ,
-    "Designed a python bot that scraps vital data from the client’s dashboard using libraries like Beautiful Soup." ,
-    "Developed a login agent API using flask which logs into the user’s account and downloads invoices with the specified ID. Deployed on AWS Lambda and currently used by the Sales team."
-  ]
+    "Developed PDF and Excel parser to extract critical data from an invoice using Java and RegEx.",
+    "Designed a python bot that scraps vital data from the client’s dashboard using libraries like Beautiful Soup.",
+    "Developed a login agent API using flask which logs into the user’s account and downloads invoices with the specified ID. Deployed on AWS Lambda and currently used by the Sales team.",
+  ];
 
   const onHoverDetails = (details) => {
     return (
-      
-        <Card variant="bordered" css={{
-          backgroundColor: "graident",
-        }}>
-          <Card.Body>
-            <ul>
-              {details.map((detail) => {
-                return <li>
-                  <Text>
-                    {detail}
-                  </Text>
-                  </li>;
-              })}
-            </ul>
-          </Card.Body>
-        </Card>
-    
+      <Card
+        css={{
+          background:
+            "linear-gradient(162deg, rgba(241,118,180,1) 23%, rgba(66,91,246,1) 73%, rgba(146,124,235,1) 83%)",
+        }}
+      >
+        <Card.Body>
+          <ul>
+            {details.map((detail) => {
+              return (
+                <li>
+                  <Text>{detail}</Text>
+                </li>
+              );
+            })}
+          </ul>
+        </Card.Body>
+      </Card>
     );
   };
 
@@ -69,168 +67,9 @@ export const Experience = () => {
           Experience
         </Text>
       </Grid.Container>
-      {/* <Grid.Container
-        justify="center"
-        css={{
-          paddingLeft: "2rem",
-          paddingRight: "2rem",
-        }}
-      >
-        <Grid
-          xs={12}
-          md={6}
-          css={{
-            padding: "0px 0px 0px 0px",
-          }}
-          justify="center"
-        >
-          <div>
-            <Text h1>
-              Summary
-              <Spacer y={1} />
-              <Text className="resume-item" h5>
-                ARGHYADEEP GHOSH
-                <Text h5>
-                  I am a computer programmer and a web developer. I am seeking a
-                  role that will allow me to demonstrate my expertise and unlock
-                  my full potential in delivering value to the company.
-                </Text>
-                <Text
-                  h6
-                  css={{
-                    fontWeight: "bold",
-                    fontStyle: "italic",
-                  }}
-                >
-                  <li>Jhargram , West Bengal , India</li>
-                  <Spacer y={1} />
-                  <li>imarghyadeep@gmail.com</li>
-                </Text>
-              </Text>
-            </Text>
-
-            <Text h1>
-              Education
-              <Spacer y={1} />
-              <Text
-                className="resume-item"
-                h5
-                css={{
-                  marginBottom: "0px",
-                }}
-              >
-                COMPUTER SCIENCE AND ENGINEERING (B.TECH)
-                <Text h5>2019-2023</Text>
-                <h5
-                  style={{
-                    fontWeight: "bold",
-                    fontStyle: "italic",
-                  }}
-                >
-                  Kalinga Institute of Industrial Technology
-                </h5>
-                <h6
-                  style={{
-                    fontWeight: "bold",
-                    fontStyle: "italic",
-                  }}
-                >
-                  CGPA- 9.23/10
-                </h6>
-              </Text>
-              <h5
-                style={{
-                  marginTop: "0px",
-                  marginBottom: "0px",
-                }}
-                className="resume-item"
-              >
-                HIGHER SECONDARY
-
-                <Text className="resume-inner-item" h5>2019</Text>
-                <Text className="resume-inner-item" h5> Vidya Bharati Chinmaya Vidyalaya</Text>
-                <Text className="resume-inner-item" h5>Percentage-85%</Text>
-              </h5>
-
-              <h5
-                style={{
-                  marginTop: "0px",
-                }}
-                className="resume-item"
-              >
-                HIGH SCHOOL
-
-                <Text h5>2017</Text>
-                <Text h5> West End High School</Text>
-                <Text h5>Percentage-88%</Text>
-              </h5>
-            </Text>
-
-            <Text h1 className="resume-item">
-              Summary
-              <h6>
-                Hey there! I'm a Full Stack Developer from India. I'm passionate
-                about building web applications and learning new technologies.
-              </h6>
-            </Text>
-
-            <Text h1 className="resume-item">
-              Summary
-              <h6>
-                Hey there! I'm a Full Stack Developer from India. I'm passionate
-                about building web applications and learning new technologies.
-              </h6>
-            </Text>
-          </div>
-        </Grid>
-
-        <Grid
-          xs={12}
-          md={6}
-          css={{
-            padding: "0px 0px 0px 0px",
-          }}
-          justify="center"
-        >
-          <div>
-            <Text h1 className="resume-item">
-              Summary
-              <h6>
-                Hey there! I'm a Full Stack Developer from India. I'm passionate
-                about building web applications and learning new technologies.
-              </h6>
-            </Text>
-
-            <Text h1 className="resume-item">
-              Summary
-              <h6>
-                Hey there! I'm a Full Stack Developer from India. I'm passionate
-                about building web applications and learning new technologies.
-              </h6>
-            </Text>
-
-            <Text h1 className="resume-item">
-              Summary
-              <h6>
-                Hey there! I'm a Full Stack Developer from India. I'm passionate
-                about building web applications and learning new technologies.
-              </h6>
-            </Text>
-
-            <Text h1 className="resume-item">
-              Summary
-              <h6>
-                Hey there! I'm a Full Stack Developer from India. I'm passionate
-                about building web applications and learning new technologies.
-              </h6>
-            </Text>
-          </div>
-        </Grid>
-      </Grid.Container> */}
       <Grid.Container>
         <VerticalTimeline lineColor={"black"}>
-        
-        <VerticalTimelineElement
+          <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{
               background: "linear-gradient(45deg,#e975a8, #726cf8)",
@@ -247,16 +86,20 @@ export const Experience = () => {
             icon={<MdOutlineCorporateFare />}
           >
             <Tooltip placement="top" content={onHoverDetails(BobbleAI)}>
-            <h3 className="vertical-timeline-element-title">Bobble AI</h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Gurugram , Haryana , India
-            </h4>
-            <p>Full Stack Developer Intern</p>
+              <Text css={{
+                color: 'white',
+              }} h3 className="vertical-timeline-element-title">Bobble AI</Text>
+              <Text css={{
+                color: 'white',
+              }} h4 className="vertical-timeline-element-subtitle">
+                Gurugram , Haryana , India
+              </Text>
+              <Text css={{
+                color: 'white',
+              }} p>Full Stack Developer Intern</Text>
             </Tooltip>
           </VerticalTimelineElement>
 
-        
-          
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{
@@ -273,13 +116,21 @@ export const Experience = () => {
             }}
             icon={<MdOutlineCorporateFare />}
           >
-            <h3 className="vertical-timeline-element-title">
+            <Tooltip placement="topStart" content={onHoverDetails(HighRadius)}>
+            <Text css={{
+                color: 'white',
+              }} h3 className="vertical-timeline-element-title">
               High Radius Corporation
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
+            </Text>
+            <Text css={{
+                color: 'white',
+              }} h4 className="vertical-timeline-element-subtitle">
               Bhubaneswar,India
-            </h4>
-            <p>Software Developer Intern</p>
+            </Text>
+            <Text css={{
+                color: 'white',
+              }} p>Software Developer Intern</Text>
+            </Tooltip>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
@@ -297,13 +148,22 @@ export const Experience = () => {
             }}
             icon={<FaUniversity />}
           >
-            <h3 className="vertical-timeline-element-title">
+            <Text css={{
+                color: 'white',
+              }} h3 className="vertical-timeline-element-title">
               Kalinga Institute Of Industrial Technology University
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
+            </Text>
+            <Text css={{
+                color: 'white',
+              }} h4 className="vertical-timeline-element-subtitle">
               Bhubaneswar, Odissa
-            </h4>
-            <p>B.Tech:- Information Technology</p>
+            </Text>
+            <Text css={{
+                color: 'white',
+              }} p>B.Tech:- Information Technology</Text>
+            <Text css={{
+                color: 'white',
+              }} p>CGPA:- 9.20</Text>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
@@ -321,14 +181,25 @@ export const Experience = () => {
             }}
             icon={<FaSchool></FaSchool>}
           >
-            <h3 className="vertical-timeline-element-title">
-              Oxford Public School
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Ranchi, Jharkhand
-            </h4>
-            <p>Intermediate</p>
-            <p>PCM</p>
+            <Text css={{
+                color: 'white',
+              }} h3 className="vertical-timeline-element-title">
+              Vidya Bharati Chinmaya Vidyalaya
+            </Text>
+            <Text css={{
+                color: 'white',
+              }} h4 className="vertical-timeline-element-subtitle">
+              Jamshedpur, Jharkhand
+            </Text>
+            <Text css={{
+                color: 'white',
+              }} p>High Secondary</Text>
+            <Text css={{
+                color: 'white',
+              }} p>PCM</Text>
+              <Text css={{
+                color: 'white',
+              }} p>Percentage : 85%</Text>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </Grid.Container>
