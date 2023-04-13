5dc2c246288e7a1bac3a053df8dd4f1ef31c01a0
Arghya721 <imarghyadeep@gmail.com>
2023-04-14 00:34:06 +0530

    Added Title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Topbar } from "./components/Topbar";
 import { Layout } from "./components/Layout";
+import { useEffect } from "react";
+
+
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Arghya's Portfolio";
+  }, []);
+
   return (
  
       <Layout>
